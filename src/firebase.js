134b4927a8a174783914590fe6638e92444538cf
Mohammed <salimmohammad7890@gmail.com>
2023-12-08 14:24:12 +0600

    added nav

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDV9WGRF1JpE-VJ4sYmb9b6_tuGcTTax_A",
+    authDomain: "netflix-clone-2eeb8.firebaseapp.com",
+    projectId: "netflix-clone-2eeb8",
+    storageBucket: "netflix-clone-2eeb8.appspot.com",
+    messagingSenderId: "693909666165",
+    appId: "1:693909666165:web:1725cec86efe02e79fbb3c"
+  };
