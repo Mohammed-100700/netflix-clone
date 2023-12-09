8d30d3227157b668ccbf06a42c00fc2d46686643
Mohammed <salimmohammad7890@gmail.com>
2023-12-10 02:16:51 +0600

    initialised the authentication

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import LoginScreen from './screens/LoginScreen';
+import React, { useEffect } from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  // const user = {
+  //   name: "sup",
+  // };
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //Logged In
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        //Logged Out
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
-
       <Router>
         {!user ? (
           <LoginScreen />
@@ -23,7 +43,6 @@ function App() {
             <Route exact path="/" element={<HomeScreen />} />
           </Routes>
         )}
-
       </Router>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDV9WGRF1JpE-VJ4sYmb9b6_tuGcTTax_A",
-    authDomain: "netflix-clone-2eeb8.firebaseapp.com",
-    projectId: "netflix-clone-2eeb8",
-    storageBucket: "netflix-clone-2eeb8.appspot.com",
-    messagingSenderId: "693909666165",
-    appId: "1:693909666165:web:1725cec86efe02e79fbb3c"
-  };
+  apiKey: "AIzaSyDV9WGRF1JpE-VJ4sYmb9b6_tuGcTTax_A",
+  authDomain: "netflix-clone-2eeb8.firebaseapp.com",
+  projectId: "netflix-clone-2eeb8",
+  storageBucket: "netflix-clone-2eeb8.appspot.com",
+  messagingSenderId: "693909666165",
+  appId: "1:693909666165:web:1725cec86efe02e79fbb3c",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
